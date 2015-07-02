06aeb71665859685d7809d6a68c55c1eaaea3fd4
Kevin Nygaard <kevin@mutablecode.com>
2015-07-02 09:22:53 -0600

    General cleanup
    - Copies docs from Plover about Gemini PR
    - Reduces scope of global variables
    - Renames variables to single convention
    - Adds ARRAY_SIZE macro

--- steno-arduino.ino
+++ steno-arduino.ino
@@ -25,8 +25,37 @@
 #define DATA_IN  4
 #define LATCH_EN 3
 #define TEST_EN  2
+#define BAUD     115200
+
+#define ARRAY_SIZE(array) \
+    (sizeof(array) / sizeof(*array))
 
 /*
+ * Setup IO and serial connections
+ */
+void setup()
+{
+    // Initialize serial
+    Serial.begin(BAUD);
+
+    // Setup IOs
+    pinMode(CLK,      OUTPUT);
+    pinMode(LATCH_EN, OUTPUT);
+    pinMode(TEST_EN,  OUTPUT);
+    pinMode(DATA_IN,  INPUT);
+
+    // Drive known values to outputs
+    digitalWrite(CLK,      LOW);
+    digitalWrite(LATCH_EN, LOW);
+    digitalWrite(TEST_EN,  LOW);
+}
+
+/*
+ * Convert raw Stentura byte data into packed Gemini PR format
+ *
+ * Data that is seemingly skipped is rendundant, and as far as I can tell,
+ * unnecessary
+ *
  * Raw matrix mapping of Stentura 200 SRT from serial shift registers
  *  0: S-
  *  1: T-
@@ -52,73 +81,54 @@
  * 21: -Z
  * 22: #
  * 23: N/A
- */
-
-// Byte array of depressed keys, following mapping above
-// 0 means not pressed, 1 means pressed
-char pressed_keys[24];
-
-// Data to send back via serial to host
-char data[6];
-
-/*
- * Setup IO and serial connections
- */
-void setup()
-{
-    // Initialize serial
-    Serial.begin(115200);
-
-    // Setup IOs
-    pinMode(CLK, OUTPUT);
-    pinMode(LATCH_EN, OUTPUT);
-    pinMode(TEST_EN, OUTPUT);
-    pinMode(DATA_IN, INPUT);
-
-    // Drive known values to outputs
-    digitalWrite(CLK,      LOW);
-    digitalWrite(LATCH_EN, LOW);
-    digitalWrite(TEST_EN,  LOW);
-}
-
-/*
- * Convert raw Stentura byte data into packed Gemini PR format
  *
- * Data that is seemingly skipped is rendundant, and as far as I can tell,
- * unnecessary
+ * Gemini PR protocol (from plover source ./plover/machine/geminipr.py)
  *
- * See plover source for more information on the Gemini PR format
+ * In the Gemini PR protocol, each packet consists of exactly six bytes and the
+ * most significant bit (MSB) of every byte is used exclusively to indicate
+ * whether that byte is the first byte of the packet (MSB=1) or one of the
+ * remaining five bytes of the packet (MSB=0). As such, there are really only
+ * seven bits of steno data in each packet byte. This is why the STENO_KEY_CHART
+ * below is visually presented as six rows of seven elements instead of six rows
+ * of eight elements.
+ * STENO_KEY_CHART = ("Fn" , "#" , "#" , "#" , "#" , "#"  , "#"  ,
+ *                    "S-" , "S-", "T-", "K-", "P-", "W-" , "H-" ,
+ *                    "R-" , "A-", "O-", "*" , "*" , "res", "res",
+ *                    "pwr", "*" , "*" , "-E", "-U", "-F" , "-R" ,
+ *                    "-P" , "-B", "-L", "-G", "-T", "-S" , "-D" ,
+ *                    "#"  , "#" , "#" , "#" , "#" , "#"  , "-Z")
+ *
  */
-void constructData()
+void construct_data(char raw_data[], char packed_data[])
 {
-    data[0] = 0x80;
-    data[1] = (pressed_keys[ 0] << 6)  /* S- */
-            | (pressed_keys[ 1] << 4)  /* T- */
-            | (pressed_keys[ 2] << 3)  /* K- */
-            | (pressed_keys[ 3] << 2)  /* P- */
-            | (pressed_keys[ 4] << 1)  /* W- */
-            | (pressed_keys[ 5] << 0); /* H- */
+    packed_data[0] = 0x80;
+    packed_data[1] = (raw_data[ 0] << 6)  /* S- */
+                   | (raw_data[ 1] << 4)  /* T- */
+                   | (raw_data[ 2] << 3)  /* K- */
+                   | (raw_data[ 3] << 2)  /* P- */
+                   | (raw_data[ 4] << 1)  /* W- */
+                   | (raw_data[ 5] << 0); /* H- */
 
-    data[2] = (pressed_keys[ 6] << 6)  /* R- */
-            | (pressed_keys[ 7] << 5)  /* A- */
-            | (pressed_keys[ 8] << 4)  /* O- */
-            | (pressed_keys[ 9] << 3); /* *  */
+    packed_data[2] = (raw_data[ 6] << 6)  /* R- */
+                   | (raw_data[ 7] << 5)  /* A- */
+                   | (raw_data[ 8] << 4)  /* O- */
+                   | (raw_data[ 9] << 3); /* *  */
 
-    data[3] = (pressed_keys[10] << 3)  /* -E */
-            | (pressed_keys[11] << 2)  /* -U */
-            | (pressed_keys[12] << 1)  /* -F */
-            | (pressed_keys[13] << 0); /* -R */
+    packed_data[3] = (raw_data[10] << 3)  /* -E */
+                   | (raw_data[11] << 2)  /* -U */
+                   | (raw_data[12] << 1)  /* -F */
+                   | (raw_data[13] << 0); /* -R */
 
-    data[4] = (pressed_keys[14] << 6)  /* -P */
-            | (pressed_keys[15] << 5)  /* -B */
-            | (pressed_keys[16] << 4)  /* -L */
-            | (pressed_keys[17] << 3)  /* -G */
-            | (pressed_keys[18] << 2)  /* -T */
-            | (pressed_keys[19] << 1)  /* -S */
-            | (pressed_keys[20] << 0); /* -D */
+    packed_data[4] = (raw_data[14] << 6)  /* -P */
+                   | (raw_data[15] << 5)  /* -B */
+                   | (raw_data[16] << 4)  /* -L */
+                   | (raw_data[17] << 3)  /* -G */
+                   | (raw_data[18] << 2)  /* -T */
+                   | (raw_data[19] << 1)  /* -S */
+                   | (raw_data[20] << 0); /* -D */
 
-    data[5] = (pressed_keys[22] << 6)  /* #  */
-            | (pressed_keys[21] << 0); /* -Z */
+    packed_data[5] = (raw_data[22] << 6)  /* #  */
+                   | (raw_data[21] << 0); /* -Z */
 }
 
 /*
@@ -126,37 +136,43 @@ void constructData()
  */
 void loop()
 {
+    // Byte array of depressed keys, following mapping above
+    // 0 means not pressed, 1 means pressed
+    static char pressed_keys[24];
+
+    // Keys packed in Gemini PR format, ready for transmission over serial
+    static char packed_keys[6];
+
+    // Chord has been started, but is not complete (all keys released)
+    static char in_progress;
+
+    // Send data to host over serial
+    static char send_data;
+
     int i;
     char pressed;
     char keys_down;
-    static char in_progress;
-    static char returnData;
 
-    if (returnData) {
-        constructData();
-        Serial.write(data[0]);
-        Serial.write(data[1]);
-        Serial.write(data[2]);
-        Serial.write(data[3]);
-        Serial.write(data[4]);
-        Serial.write(data[5]);
+    if (send_data) {
+        construct_data(pressed_keys, packed_keys);
+        Serial.write(packed_keys, ARRAY_SIZE(packed_keys));
 
         /*
-         * Data returned to host, reset statful variables
-         * Note that we don't need to do this in the setup, becasue they are all
+         * Data returned to host, reset stateful variables
+         * Note that we don't need to do this in the setup, because they are all
          * static and guaranteed to be 0 by spec
          */
-        memset(pressed_keys, 0, 23);
-        returnData  = 0;
+        memset(pressed_keys, 0, sizeof(pressed_keys));
+        send_data   = 0;
         in_progress = 0;
 
     } else {
-        // Latch data
+        // Latch current state of all keys
         digitalWrite(LATCH_EN, HIGH);
 
         // Read all latched data
         keys_down = 0;
-        for (i = 0; i < 24; i++) {
+        for (i = 0; i < ARRAY_SIZE(pressed_keys); i++) {
             /*
              * All inputs are pulled up. Pressing a key shorts the circuit to
              * ground.
@@ -165,6 +181,8 @@ void loop()
              * logic.
              */
             pressed          = !digitalRead(DATA_IN);
+
+            // Once a key is pressed, it stays pressed until the chord is over
             pressed_keys[i] |= pressed;
             if (pressed) {
                 keys_down   = 1;
@@ -184,8 +202,7 @@ void loop()
 
         // Return data to host when all keys have been released
         if (in_progress && !keys_down) {
-            returnData = 1;
+            send_data = 1;
         }
     }
 }
-
